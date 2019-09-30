5acbe71809753a989f882b0e94c7300591f5cdc9
anthonypitts2022 <anthony.pitts@columbia.edu>
2019-09-30 12:09:15 -0400

    initial work on the backend for the contactlist;mostly set up file structure, started mongoDB, and config for Contacts backend microservice.

---
+++ app/services/contacts/contacts-service.js
@@ -0,0 +1,200 @@
+contactsprocess.env.NODE_ENV = process.NODE_ENV || "development";
+const rootPath = require("app-root-path");
+require("module-alias/register");
+const { logger } = require("@lib/logger.js");
+const mongoose = require("./config/mongoose.js");
+const express = require("./config/express.js");
+const multer = require('multer');
+const { createApolloFetch } = require('apollo-fetch');
+const shortid = require("shortid");
+var path = require('path');
+
+const db = mongoose();
+const app = express();
+const port = process.env.PORT || 3301;
+
+
+
+//============  Uploading images with contacts   ====================//
+
+app.route('/upload').post(function(req, res) {
+
+  //generates the image file id
+  const fileId = shortid.generate();
+
+  var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, "uploads")
+      },
+      filename: function (req, file, cb) {
+        //console.log(file);
+        //if no file was provided
+        //if(file==undefined){
+        //  cb(null, null);
+        //}
+        //else{
+          cb(null, fileId + path.extname(file.originalname) );
+        //}
+      }
+  });
+
+  var upload = multer({ storage: storage }).single('file');
+
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+       res.status(500).send({errors: err});
+    } else if (err) {
+       console.log(err)
+       res.status(500).send({errors: err});
+    }
+    //if no file was provided/uploaded
+    if(req.file==undefined){
+      return res.status(200).send({errors: "contact must include a file. No file was provided."})
+    }
+
+    //if no errors on uploading file, proceed to create contact
+
+    //calls create contact database mutation
+    var fetch = createApolloFetch({
+      uri: "http://localhost:3301/contacts"
+    });
+    //binds the res of upload to fetch to return the fetch data
+    fetch = fetch.bind(res)
+    fetch({
+      query:
+      `
+        mutation createContact($input: createContactInput){
+          Contact: createContact(input: $input){
+            errors{
+              msg
+            }
+            fileId
+            fileType
+            userId
+            id
+            caption
+            likeCount
+            dislikeCount
+            comments{
+              text
+              userId
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+           caption : req.body.caption,
+           fileId : fileId,
+           fileType : path.extname(req.file.originalname)
+         }
+      }
+  })
+  .then(result => {
+    //result.data holds the data returned from the createContact mutation
+    return res.status(200).send(result.data.Contact);
+  })
+  })
+});
+
+//=========================================================================//
+
+//============  create like mutation call   ====================//
+
+app.route('/createlike').post(function(req, res) {
+
+  //calls create like database mutation
+  var fetch = createApolloFetch({
+    uri: "http://localhost:3301/contacts"
+  });
+  //binds the res of upload to fetch to return the fetch data
+  fetch = fetch.bind(res)
+  fetch({
+    query:
+    `
+      mutation createLike($input: createLikeInput){
+        Like: createLike(input: $input){
+          errors{
+            msg
+          }
+          id
+          userId
+          contactId
+          isLike
+        }
+      }
+    `,
+    variables: {
+      input: {
+        isLike: req.body.input.isLike,
+        contactId: req.body.input.contactId
+      }
+    }
+  })
+  .then(result => {
+    //result.data holds the data returned from the createLike mutation
+    return res.status(200).send(result.data.Like);
+  })
+});
+
+//=========================================================================//
+
+//============  get contact mutation call   ====================//
+
+app.route('/querycontact').post(function(req, res) {
+
+
+  //calls create like database mutation
+  var fetch = createApolloFetch({
+    uri: "http://localhost:3301/contacts"
+  });
+  //binds the res of upload to fetch to return the fetch data
+  fetch = fetch.bind(res)
+  fetch({
+    query:
+    `
+    query getAContact($input: getContactInput!){
+      Contact: getAContact(input: $input){
+        errors{
+          msg
+        }
+        fileId
+        fileType
+        userId
+        id
+        caption
+        likeCount
+        dislikeCount
+        comments{
+          text
+          userId
+        }
+      }
+    }
+    `,
+    variables: {
+      input: {
+        contactId: req.body.input.contactId
+      }
+    }
+  })
+  .then(result => {
+    //result.data.contact holds the data returned from the getAcontact mutation
+    return res.status(200).send(result.data.Contact);
+  })
+});
+
+//=========================================================================//
+
+//============  Serving files of contacts   ====================//
+
+
+app.route("/file/:fileId/:extension").get(function (req, res) {
+  res.sendFile(req.params.fileId + req.params.extension, { root: "./uploads"});
+});
+
+
+//============================================================//
+
+
+app.listen(port, () => logger.info(`contacts service started on port ${port}`));
