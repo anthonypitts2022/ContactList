51b0bdbd486452fe791914c2df75365e7b63921d
anthonypitts2022 <anthony.pitts@columbia.edu>
2019-10-02 21:44:13 -0400

    Fixed navbar for My Contacts and Add Contact button

--- app/client/src/components/myContactsBar.jsx
+++ app/client/src/components/myContactsBar.jsx
@@ -5,31 +5,8 @@ class MyContactsBar extends Component {
   render(){
     return(
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">My Contacts</a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <div className="container">
-              <a className="btn btn-outline-success my-2 my-sm-0" href="/addcontact" role="button">Add Contact</a>
-              <div className="modal" id="myModal" role="dialog">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <button type="button" className="close" data-dismiss="modal">&times;</button>
-                      <h4 className="modal-title">Modal Header</h4>
-                    </div>
-                    <div className="modal-body">
-                      <p>Some text in the modal.</p>
-                    </div>
-                    <div className="modal-footer">
-                      <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+        <a className="navbar-brand" >My Contacts</a>
+        <a className="btn btn-outline-success my-2 my-sm-0" href="/addcontact" role="button">Add Contact</a>
       </nav>
     );
   }
